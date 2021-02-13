6dc8ff234fd8b3fce920f39e586ed86727cf0734
boxsie <dan@boxsie.co.uk>
2021-02-13 14:18:14 +0000

    added env var for port

--- BotData.Api/DbCreds.cs
+++ BotData.Api/DbCreds.cs
@@ -5,15 +5,19 @@ namespace BotData.Api
     public class DbCreds
     {
         public string Url { get; set; }
+        public string Port { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string Database { get; set; }
 
-        public string ConnectionString => $"Host={Url};Username={Username};Password={Password};Database={Database};Port=3306";
+        public string ConnectionString => $"Host={Url};Username={Username};Password={Password};Database={Database};Port={Port}";
 
         public DbCreds()
         {
-            Url = Environment.GetEnvironmentVariable("DB_URL");
+            var full_url = Environment.GetEnvironmentVariable("DB_URL");
+            var urlPort = full_url.Split(':');
+            Url = urlPort[0];
+            Port = urlPort[1];
             Username = Environment.GetEnvironmentVariable("DB_USER");
             Password = Environment.GetEnvironmentVariable("DB_PASS");
             Database = Environment.GetEnvironmentVariable("DB_NAME");
